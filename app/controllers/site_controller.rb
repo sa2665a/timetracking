995beb4bee032fe136f561b7eae0ba80f94ef0d4
Simon Acosta <acosta1204@gmail.com>
2016-11-14 11:12:58 +0100

    params exercises

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,8 +2,20 @@ class SiteController < ApplicationController
 	def home
 		
 	end
+
 	def contact
 		
+	end
+
+	def calculator
+
+	end
+
+	def calculate 
+		@num1 = params[:first_num].to_f
+		@num2 = params[:second_num].to_f
+		@result = @num1 + @num2
+		render 'calculate'
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 	get'/', to: 'site#home'
 	get '/contact', to: "site#contact"
+	get '/calculator', to: 'site#calculator'
+	post '/calculate', to: 'site#calculate'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
