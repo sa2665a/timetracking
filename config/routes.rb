e0eba3edd75d94b54863e9026172d60a5d8a93f9
Simon Acosta <acosta1204@gmail.com>
2016-11-15 12:47:15 +0100

    Added the views table of projects

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	get '/contact', to: "site#contact"
 	get '/calculator', to: 'site#calculator'
 	post '/calculate', to: 'site#calculate'
+	get '/projects', to: 'projects#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
