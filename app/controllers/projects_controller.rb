2611f627fd9cf8f40947e9b98ead18c4eee7c558
Simon Acosta <acosta1204@gmail.com>
2016-11-16 10:37:08 +0100

    create action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,22 @@ class ProjectsController < ApplicationController
 		unless @my_project
 			render "project_not_found"
 		end
+
+	end
+
+	def new
+		@my_project = Project.new
+	render 'new'
+	end
+
+	def create
+
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:description => params[:project] [:description])
+		@my_project.save
+
+		redirect_to "/projects/#{@my_project.id}"
 	end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
 
 	get '/calculator', to: 'site#calculator'
 	post '/calculate', to: 'site#calculate'
-	
-	get '/projects', to: 'projects#index'
-	get '/projects/:id', to: 'projects#show'
 
+	get '/projects', to: 'projects#index'
+
+	get '/projects/new', to: 'projects#new'
+
+	get '/projects/:id' , to: 'projects#show'
+
+	post '/projects', to: 'projects#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
